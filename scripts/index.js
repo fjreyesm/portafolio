50a84e9f8a6e2205740645cedc138847701416ec
fjreyesm <franklin.reyes.2003@gmail.com>
2022-05-28 22:57:15 +0200

    menu horizontal desplegable abre y cierra

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,31 @@
 console.log("Hola mundo");
 // elemento
 
-const elemento = document.getElementsByClassName("opcion_menu");
+const elemento = document.querySelector("input[type='checkbox'] ");
+const menuDesp = document.getElementById("closem");
+// evento
+elemento.addEventListener("click", openMenu);
+
+//ejecucion
+function openMenu(event) {
+  console.log(menuDesp);
+  console.log("Abrir menu");
+  menuDesp.classList.add("open-menu");
+}
+
+//elemento
+//const cerrarOption = document.getElementsByClassName("opcion_menu")[0];
+const prueba = document.getElementById("closem");
 
 // evento
-const evento = document.addEventListener("click", function (e) {
-  console.log(e.target);
-  document.body.classList.add("close-menu");
-});
+//cerrarOption.addEventListener("click", closeMenu);
+prueba.addEventListener("click", closeMenu);
+
+//ejecucion
+function closeMenu(event) {
+  console.log("cerrar");
+  console.log(event);
+  menuDesp.classList.remove("open-menu");
+  menuDesp.classList.add("close-menu");
+  menuDesp.classList.remove("close-menu");
+}
